6da3dec2f33c7e0d91abcc100dbe9fc34dab2233
FUNExtreme <robin.maenhaut.coder@gmail.com>
2017-09-20 21:04:56 +0200

    Form validation - Name -- required - Description -- Required -- Minimum length: 5 -- Maximum length: 50 - Ingredients -- At least one should be selected - Base Price --Required

--- src/app/pizzeria.controller.js
+++ src/app/pizzeria.controller.js
@@ -5,7 +5,19 @@ import { PizzaService } from './pizza.service';
 export function PizzeriaController(pizzaService) {
     var vm = this;
     vm.pizzas = pizzaService.getPizzas();
+    
     vm.ingredients = pizzaService.getIngredients();
+    vm.isAnIngredientSelected = function(obj) {
+        console.log("called");
+        console.log(vm.ingredients);
+        for(var x = 0; x < vm.ingredients.length; x ++) {
+            if(vm.ingredients[x].selected) {
+                console.log("selected");
+                return true;
+            }
+        }
+        return false;
+    }
 
     vm.selectedPizza = undefined;
     vm.onPizzaClick = function(pizza) {
